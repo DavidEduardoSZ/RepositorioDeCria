9cefb429a584c6c42b3575b0fcce35ac6dab623e
DAVID EDUARDO SANTANA ZANGEACOMO <david.eszangeacomo@senacsp.edu.br>
2026-02-26 11:51:01 -0300

    Atividade atualizada

--- config/conexao.php
+++ config/conexao.php
@@ -1,24 +1,24 @@
 <?php
-
+ 
 class Conexao
 {
-
+ 
   //variavel de conexao
-  private $host = '10.91.45.44';
-  private $bd = 'imobiliaria';
-  private $user = 'admin';
-  private $pass = '123456';
-
+  private $host = '10.91.45.43'; //IP DO BANCO DE DADOS
+  private $db = 'imobiliaria'; // NOME DO BANCO DE DADOS
+  private $user = 'admin'; // USUARIO
+  private $pass = '123456'; // SENHA
+ 
   public function conexao()
   {
-
+ 
     try {
       //configura a conexao com as informacoes do banco de dados
-
-      $pdo = new PDO("mysql:host={$this->host};bdname={$this->bd};charset=utf8", $this->user, $this->pass);
-      //configura o PDO para lancar os erros como excessoes   
+ 
+      $pdo = new PDO("mysql:host={$this->host};dbname={$this->db};charset=utf8", $this->user, $this->pass);
+      //configura o PDO para lancar os erros como excessoes  
       $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
+ 
       return $pdo;
     } catch (PDOException $err) {
       die("Erro na conexão" . $err->getMessage());
@@ -26,4 +26,4 @@ class Conexao
     }
   }
 }
-?>
+ 
--- model/usuario.php
+++ model/usuario.php
@@ -1,110 +1,219 @@
 <?php
-
 require_once(__DIR__ . "/../config/conexao.php");
-
+ 
 class Usuario
 {
-    private int $id;
-    private string $nome;
-    private string $email;
-    private string $senhaHash;
-    private int $id_perfil;
-    private bool   $ativo;
-
+ 
+    private ?int     $id;
+    private string  $nome;
+    private string  $email;
+    private string  $senhaHash;
+    private int     $idPerfil;
+    private bool    $ativo;
+ 
+    private ?string $perfilNome = null;
+ 
+ 
     public function __construct(
-
-        int $id = 0,
+        ?int $id = 0,
         string $nome,
         string $email,
         string $senhaHash,
         int $idPerfil,
-        bool $ativo,
-
+        ?bool $ativo = true,
     ) {
         $this->id = $id;
         $this->nome = $nome;
         $this->email = $email;
-        $this->senhaHash = password_hash($senhaHash, PASSWORD_DEFAULT);
-        $this->$idPerfil = $idPerfil;
+        $this->senhaHash = $senhaHash;
+        $this->idPerfil = $idPerfil;
         $this->ativo = $ativo;
     }
-
-    //Metodo magico GET
+ 
+    // Métodos mágicos Get e Set
     public function __get(string $prop)
     {
         if (property_exists($this, $prop)) {
             return $this->$prop;
         }
-        throw new Exception("Propriedade {$prop} não existe");
+        throw new  Exception("Propriedade {$prop} não existe");
     }
-
-
+ 
+ 
     public function __set(string $prop, $valor)
     {
         switch ($prop) {
             case "id":
                 $this->id = (int)$valor;
                 break;
-
             case "nome":
                 $this->nome = trim($valor);
                 break;
-
             case "email":
-                if (filter_var($valor, FILTER_VALIDATE_EMAIL)) {
-                    throw new Exception("E-mail invalido");
+                if (!filter_var($valor, FILTER_VALIDATE_EMAIL)) {
+                    throw new Exception("E-mail inválido");
                 }
                 $this->email = $valor;
                 break;
-
             case "senhaHash":
                 $this->senhaHash = password_hash($valor, PASSWORD_DEFAULT);
                 break;
-
             case "idPerfil":
                 $this->idPerfil = $valor;
                 break;
-
             case "ativo":
                 $this->ativo = (bool)$valor;
                 break;
-
+ 
+            case "perfilNome";
+                $this->perfilNome = $valor;
+                break;
             default:
-                throw new Exception("Propriedade ($prop) não permitida");
+                throw new Exception("Propriedade {$prop} não permitida");
         }
     }
-
+ 
     private static function getConexao()
     {
         return (new Conexao())->conexao();
     }
-
+ 
     public function inserir()
     {
         $pdo = self::getConexao();
-
-        $sql = "INSERT INTO `usuarios` (`nome`,`email`,`senha`,`ativo`,`id_perfil`)
-    VALUES (:nome, :email, :senha, :ativo, :idPerfil)";
-
+ 
+        $sql = "INSERT INTO `usuarios` (`nome`, `email`, `senha`, `ativo`, `id_perfil`)
+        VALUES (:nome, :email, :senha, :ativo, :idPerfil)";
+ 
         $stmt = $pdo->prepare($sql);
-
+ 
         $stmt->execute([
-            ':nome' => $this->nome,
-            ':email' => $this->email,
-            ':senha' => $this->senha,
-            ':ativo' => $this->ativo ? 1: 0,
-            ':idPerfil' => $this->idPerfil,
-
-
-
+            ':nome'     => $this->nome,
+            ':email'    => $this->email,
+            ':senha'    => $this->senhaHash,
+            ':ativo'    => $this->ativo,
+            ':idPerfil' =>  $this->idPerfil,
+ 
         ]);
-
+ 
         $ultimoId = $pdo->lastInsertId();
+ 
+        if ($ultimoId <= 0) {
+            throw new Exception("Não Foi Possivel inserir o usuário");
+        }
+        return $ultimoId;
+    }
+ 
+    public static function listar()
+    {
+        $pdo = self::getConexao();
+ 
+        $sql = "SELECT u.id_usuario,
+            u.nome,
+            u.email,
+            u.ativo,
+            u.id_perfil,
+            p.nome_perfil as perfil_nivel
+        FROM usuarios as u
+        INNER JOIN perfis p on p.id_perfil = u.id_perfil
+        ORDER BY u.nome";
+ 
+        $stmt = $pdo->query($sql);
+ 
+        $usuarios = [];
+ 
+        //Retornando todos usuarios
+ 
+        $usuarios = [];
+ 
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+ 
+            $usuario = new Usuario(
+                id: $row['id_usuario'],
+                nome: $row['nome'],
+                email: $row['email'],
+                senhaHash: "",
+                idPerfil: $row['id_perfil'],
+                ativo: (bool)$row['ativo']
+            );
+ 
+            $usuario->perfilNome = $row['perfil_nivel'];
+ 
+            array_push($usuarios, $usuario);
+ 
+            $lista = $usuarios;
+        }
+ 
+        return $usuarios;
+    }
+ 
+    public static function buscarPorId(int $id)
+    {
+ 
+        $pdo = self::getConexao();
+ 
+        $sql = "SELECT u.id_usuario,
+            u.nome,
+            u.email,
+            u.ativo,
+            u.id_perfil,
+            p.nome_perfil as perfil_nivel
+            FROM usuarios as u
+            INNER JOIN perfis p on p.id_perfil = u.id_perfil
+            WHERE u.id_usuario = :id";
+ 
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute([':id' => $id]);
+
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+ 
+        if(!$row){
+            throw new Exception("ID do usuário não encontrado.");
+            return null;
+        }
+
+      
+
+        $usuario = new Usuario(
+            id: $row['id_usuario'],
+            nome: $row['nome'],
+            email: $row['email'],
+            senhaHash: "",
+            idPerfil: $row['id_perfil'],
+            ativo: (bool)$row['ativo']
+        );
+ 
+        $usuario->perfilNome = $row['perfil_nivel'];
+ 
+ 
+        return $usuario;
     }
 }
+ 
+// ///--- Teste inserir usuario
+// $usuario1 = (new Usuario(
+//     nome:       "Natan",
+//     email:      "natan@gmail.com",
+//     senhaHash:  "123",
+//     idPerfil:   "3",
+//     ativo:      true
+// ))->inserir();
+ 
+// $usuario1->nome = "Apollo David";
+// echo $usuario1->nome;
+// echo $usuario1->senhaHash ."<br>";
+// $usuario1->senhaHash = 123;
+// echo $usuario1->senhaHash;
+ 
+ 
+echo "<pre>";
 
-$usuario1 = new Usuario(nome: "Apollo ", email: "apollo@gmail.com", senhaHash: "123", ativo: false, idPerfil: 1);
 
-$usuario1->nome = "Apollo David";
-echo $usuario1->nome;
-echo $usuario1->senhaHash;
+try{
+    print_r(Usuario::buscarPorId(1));
+} 
+catch(Exception $err){
+    echo $err->getMessage();
+}
+
+?>
